6afd2201f0932605fdf89d98aea81ffa1aecb138
s52cheng <siqi.cheng@mail.mcgill.ca>
2025-04-27 11:25:27 -0400

    Add files via upload

--- data.py
+++ data.py
@@ -83,14 +83,16 @@ def trainGenerator(batch_size,train_path,image_folder,mask_folder,aug_dict,image
 
 
 
-# def testGenerator(test_path,num_image = 30,target_size = (256,256),flag_multi_class = False,as_gray = True):
-#     for i in range(num_image):
-#         img = io.imread(os.path.join(test_path,"%d.png"%i),as_gray = as_gray)
-#         img = img / 255
-#         img = trans.resize(img,target_size)
-#         img = np.reshape(img,img.shape+(1,)) if (not flag_multi_class) else img
-#         img = np.reshape(img,(1,)+img.shape)
-#         yield (img,)
+def testGenerator2(test_path,num_image = 30,target_size = (256,256),flag_multi_class = False,as_gray = True):
+    for i in range(num_image):
+        img = io.imread(os.path.join(test_path,"%d.png"%i),as_gray = as_gray)
+        img = img / 255
+        img = trans.resize(img,target_size)
+        img = np.reshape(img,img.shape+(1,)) if (not flag_multi_class) else img
+        img = np.reshape(img,(1,)+img.shape)
+        yield (img,)
+
+
 
 def testGenerator(test_path, num_image=30, target_size=(256,256), flag_multi_class=False, as_gray=True):
     image_folder = os.path.join(test_path, "image")
